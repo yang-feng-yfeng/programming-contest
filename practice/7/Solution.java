8ecc423d08197a57f12cee53f1dea1c444115767
Yang Feng <yang.feng.yfeng@gmail.com>
2021-02-02 11:29:36 +0100

    Add Solution 7: reverse integer using String/StringBuilder

---
+++ practice/7/Solution.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/reverse-integer/
+class Solution {
+    public int reverse(int x) {
+        StringBuilder result = new StringBuilder();
+        if (x < 0) {
+            x = -x;
+            result.append('-');
+        }
+        
+        while (x != 0) {
+            int mod = x % 10;
+            x = x / 10;
+            result.append(mod);
+        }
+        
+        int res;
+        try {
+           res = Integer.parseInt(result.toString());
+        }
+        catch (NumberFormatException e)
+        {
+           res = 0;
+        }
+        return res;
+    }
+}
