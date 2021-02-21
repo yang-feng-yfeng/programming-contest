4a3faeac0022cb9ccd76d9ba4d78829d75cc1736
Yang Feng <yang.feng.yfeng@gmail.com>
2021-02-21 23:37:42 +0100

    [Greedy, w46] Add solution 1764, use greedy to find the first occurance of the element

--- src/main/java/lcode/w46/Solution1764.java
+++ src/main/java/lcode/w46/Solution1764.java
@@ -1,2 +1,38 @@
-package lcode.w46;public class Solution1764 {
+package lcode.w46;
+
+// https://leetcode.com/problems/form-array-by-concatenating-subarrays-of-another-array/
+public class Solution1764 {
+    public boolean canChoose(int[][] groups, int[] nums) {
+        int i = 0;
+        int j;
+        int found = 0;
+        boolean noFound = false;
+
+        for (int[] group : groups) {
+            int size = group.length;
+            while (i < nums.length) {
+                if (nums[i] == group[0]) {
+                    for (j = i; j < i + size && j < nums.length && !noFound; j++) {
+                        if (group[j - i] != nums[j]) {
+                            noFound = true;
+                            i++;
+                        }
+                    }
+                    if (!noFound && (j != i + size)) {
+                        return false;
+                    }
+                    if (!noFound) {
+                        i = i + size;
+                        found++;
+                        noFound = false;
+                        break;
+                    }
+                    noFound = false;
+                } else {
+                    i++;
+                }
+            }
+        }
+        return found == groups.length;
+    }
 }
