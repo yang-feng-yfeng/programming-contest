988c797bdb6c59157ff426024a3b86d9e739c6e1
Yang Feng <yang.feng.yfeng@gmail.com>
2021-03-17 00:49:53 +0100

    [codejam 2020 r1b] add solution A

--- src/main/java/codejam/y2020/r1b/a/Solution.java
+++ src/main/java/codejam/y2020/r1b/a/Solution.java
@@ -1,7 +1,8 @@
 package codejam.y2020.r1b.a;
 // Remove package line for submit
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Solution {
@@ -15,157 +16,64 @@ class Solution {
         for (int i = 0; i < T; i++) {
             int X = in.nextInt();
             int Y = in.nextInt();
-            // Reverse the direction at the end
-            String binaryX = X > 0 ? Integer.toBinaryString(X) : Integer.toBinaryString(-X);
-            String binaryY = Y > 0 ? Integer.toBinaryString(Y) : Integer.toBinaryString(-Y);
 
-            int diff = binaryX.length() - binaryY.length();
-            if (diff > 0) {
-                binaryY = "0".repeat(diff) + binaryY;
-            } else if (diff < 0) {
-                binaryX = "0".repeat(-diff) + binaryX;
-            }
+            int unsignedX = X > 0 ? X : -X;
+            int unsignedY = Y > 0 ? Y : -Y;
 
-
-            boolean[] bx = new boolean[binaryX.length() + 1];
-            for (int i1 = 1; i1 <= binaryX.length(); i1++) {
-                bx[i1] = binaryX.charAt(i1-1) == '1';
-            }
-            boolean[] by = new boolean[binaryY.length() + 1];
-            for (int i1 = 1; i1 <= binaryY.length(); i1++) {
-                by[i1] = binaryY.charAt(i1-1) == '1';
-            }
-
-            int maxLength = Math.max(binaryX.length(), binaryY.length());
-
-            int[] resultX = new int[maxLength + 1];
-            int[] resultY = new int[maxLength + 1];
-
-
-
-            String res = "";
-            boolean xSurplus=false, ySurplus=false;
-
-            for (int index = maxLength; index >= 0 && res.isEmpty(); index--) {
-                if (index == maxLength) {
-                    // last digit
-                    if ((bx[index] & by[index]) || (!bx[index] & !by[index])) {
-                        res = "IMPOSSIBLE";
-                    }
-                    else if (bx[index]) {
-                        resultX[index] = 1;
-                    } else {
-                        resultY[index] = 1;
-                    }
-                } else if (index != 0){
-                    if ((!bx[index] & !by[index] )) {
-                        if ( (!xSurplus && !ySurplus) || xSurplus && ySurplus) {
-                            res = "IMPOSSIBLE";
-                        } else {
-                            if (xSurplus) {
-                                resultX[index] = 1;
-                                xSurplus = false;
-                            } else {
-                                resultY[index] = 1;
-                                ySurplus = false;
-                            }
-                        }
-                    } else if (bx[index] & by[index]) {
-                        if (xSurplus && ySurplus) {
-                            res = "IMPOSSIBLE";
-                        } else if (!bx[index + 1]) {
-                            resultX[index] = 1;
-                            int k = index;
-                            while (k < resultY.length && resultY[k] == 0 ) {
-                                k++;
-                            }
-                            if (resultY[k] == 1) {
-                                resultY[k] = -1;
-                                ySurplus = true;
-                            } else if (resultY[k] == 0){
-                                res = "IMPOSSIBLE";
-                            }
-
-                        } else if (!by[index + 1]) {
-                            resultY[index] = 1;
-                            int k = index;
-                            while (k < resultX.length && resultX[k] == 0 ) {
-                                k++;
-                            }
-                            if (resultX[k] == 1) {
-                                resultX[k] = -1;
-                                xSurplus = true;
-                            } else if (resultX[k] == 0){
-                                res = "IMPOSSIBLE";
-                            }
-                        } else {
-                            // next digit all 1
-                            int nextIdx = 1;
-                            while (bx[index + nextIdx] && by[index + nextIdx]) {
-                                nextIdx ++;
-                            }
-                            if (!bx[index + nextIdx]) {
-                                resultX[index] = 1;
-                            } else {
-                                resultY[index] = 1;
-                            }
-                        }
-                    } else if (bx[index]) {
-                        if (ySurplus) {
-                            res = "IMPOSSIBLE";
-                        } else {
-                            resultX[index] = 1;
-                        }
-                    } else {
-                        if (xSurplus) {
-                            res = "IMPOSSIBLE";
-                        } else {
-                            resultY[index] = 1;
-                        }
-                    }
-                } else if (index == 0) {
-                    if (xSurplus && ySurplus) {
-                        res = "IMPOSSIBLE";
-                    } else if (xSurplus) {
-                        resultX[0] = 1;
-                    } else if (ySurplus){
-                        resultY[0] = 1;
-                    }
-                }
-            }
-            String result = writeRes(res, resultX, resultY, X >= 0, Y >= 0);
-
-            System.out.println("Case #" + (i+1) + ": " + result);
+            List<Integer> steps = new ArrayList<>();
+            boolean ok = validate(unsignedX, unsignedY, steps);
+            System.out.println("Case #" + (i+1) + ": " + (ok ? fillResult(steps, X>0, Y>0) :"IMPOSSIBLE"));
         }
         in.close();
     }
 
-    private static String writeRes(String res, int[] resultX, int[] resultY, boolean isXPositive, boolean isYPositive) {
-        char[] result = new char[resultX.length];
-        if (res.equals("IMPOSSIBLE")) {
-            return res;
-        } else {
-            for (int i = resultX.length - 1; i >= 0 ; i--) {
-                int dir = resultX[i] * (isXPositive ? 1: -1);
-                if (dir == 1) {
-                    result[result.length - i -1] = 'E';
-                } else if (dir == -1) {
-                    result[result.length - i -1] = 'W';
-                }
+    private static String fillResult(List<Integer> steps, boolean isPositiveX, boolean isPositiveY) {
+        StringBuilder sb = new StringBuilder();
+        for (Integer step : steps) {
+            if (step == 1 || step == -1) {
+                sb.append( (isPositiveX? 1*step : -1*step) > 0 ? "E": "W" );
+            } else {
+                sb.append( (isPositiveY? 1*step : -1*step) > 0 ? "N": "S" );
+            }
+        }
+        return sb.toString();
+    }
 
-            }
-            for (int i = resultY.length - 1; i >=0  ; i--) {
-                int dir = resultY[i] * (isYPositive ? 1: -1);
-                if (dir == 1) {
-                    result[result.length - i -1] = 'N';
-                } else if (dir == -1) {
-                    result[result.length - i -1] = 'S';
+    private static boolean validate(int x, int y, List<Integer> steps) {
+        if (x == 0 && y == 0) {
+            return true;
+        }
+        if (x == 1 && y == 0) {
+            steps.add(1);
+            return true;
+        }
+        if ( x == 0 && y == 1 ) {
+            steps.add(2);
+            return true;
+        }
+
+        if ( (x % 2 + y % 2) == 1 ) {
+            if (x % 2 == 1) {
+                steps.add(-1); // W
+                if (validate((x+1)/2, y/2, steps)) {
+                    return true;
+                } else {
+                    steps.remove(steps.size() - 1);
+                    steps.add(1); // E
+                    return validate((x - 1) / 2, y / 2, steps);
+                }
+            } else {
+                steps.add(-2); // S
+                if (validate(x/2, (y+1)/2, steps)) {
+                    return true;
+                } else {
+                    steps.remove(steps.size() - 1);
+                    steps.add(2); // N
+                    return validate(x/ 2, (y-1) / 2, steps);
                 }
             }
-            if (result[result.length - 1] == '\0') {
-                return new String(Arrays.copyOfRange(result, 0, result.length -1));
-            }
-            return new String(result);
+        } else {
+            return false;
         }
     }
 }
